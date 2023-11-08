bafdae94b843762350c98350a0e4cc7f31d7c625
Mingyan Zhu <hiterzmy@outlook.com>
2023-11-08 21:12:07 +0800

    [-] fixed some bugs

--- test.py
+++ test.py
@@ -1,31 +1,21 @@
-# import tensorflow as tf
-# import tensorflow.keras as keras
-from utils import *
+from utils import AUROC_Score
 import numpy as np
-
-
-
 
 
 def process(file_data):
     a = np.load(file_data)[:3000]
-   
+
     score = np.empty(len(a))
 
-    for i in range(len(a))
+    for i in range(len(a)):
         score[i] = np.mean(a[i] == a[i][0])
 
     return score
 
 
-
-
-
-
-if __name__ == '__main__':
-
-
-    
-    AUROC_Score(process("saved_np/WaNet/tiny_bd.npy"),
-               process("saved_np/WaNet/tiny_benign.npy"), "tiny_imagenet")
-  
+if __name__ == "__main__":
+    AUROC_Score(
+        process("saved_np/WaNet/tiny_bd.npy"),
+        process("saved_np/WaNet/tiny_benign.npy"),
+        "tiny_imagenet",
+    )
